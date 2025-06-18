abc5d4d3c669974aefeef33048da10058bfd4277
aibek <ajbekmannapov@gmail.com>
2025-06-18 10:40:20 +0600

    edit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 // src/pages/_app.tsx
 import type { AppProps } from "next/app";
-import "@/styles/globals.css";
+import "@/app/globals.scss";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
